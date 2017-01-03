123a24e50ba33417167aa8d358452afd319f527a
Yury Dymov <yuri.dymov@sap.com>
2017-01-03 13:10:24 +0100

    webpack cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -39,24 +39,6 @@ module.exports = {
   },
   module: {
     loaders: [
-      /*
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
-      },
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!less-loader')
-      },
-      { test: /\.gif$/, loader: 'url-loader?limit=10000&mimetype=image/gif' },
-      { test: /\.jpg$/, loader: 'url-loader?limit=10000&mimetype=image/jpg' },
-      { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
-      { test: /\.svg/, loader: 'url-loader?limit=26000&mimetype=image/svg+xml' },
-      { test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1' },
-      { test: /\.jss?x?l?$/, loader: process.env.NODE_ENV !== 'production' ? 'babel!eslint-loader' : 'babel', exclude: [/node_modules/, /public/] },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.txt$/, loader: 'raw-loader' }
-      */
       { test: /\.jss?x?l?$/, loader: process.env.NODE_ENV !== 'production' ? 'babel!eslint-loader' : 'babel', exclude: [/node_modules/, /public/] },
     ]
   },
